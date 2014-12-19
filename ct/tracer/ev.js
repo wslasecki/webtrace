4fdc84216449aba3d6726bbf2bddf2db1765f4d9
Walter S. Lasecki <wlasecki@roc.cs.rochester.edu>
2014-12-18 21:28:44 -0500

    fetch active element on input event

--- ct/tracer/ev.js
+++ ct/tracer/ev.js
@@ -11,26 +11,27 @@ $(document).ready( function() {
 
 //$('#content_frame').load( function() {
 function loadInit() {
-    // Handle input events
-    $('#content_frame').contents().find('input').click( function(ev) {
-      processInputEv(ev);
-    });
-    $('#content_frame').contents().find('input').keydown( function(ev) {
-      processInputEv(ev);
-    });
-    $('#content_frame').contents().find('button').click( function(ev) {
-      processInputEv(ev);
-    });
+  // Handle input events
+  $('#content_frame').contents().find('input').click( function(ev) {
+    processInputEv(ev);
+  });
+  $('#content_frame').contents().find('input').keydown( function(ev) {
+    processInputEv(ev);
+  });
+  $('#content_frame').contents().find('button').click( function(ev) {
+    processInputEv(ev);
+  });
 
-    // Handle page loads
-    //$('#content_frame').onbeforeunload( function() {
-    //$('#content_frame').unload( function() {
-    $('#content_frame').bind('beforeunload', function() {
-      processPageUnloadEv();
-    });
-    $('#content_frame').on('load', function() {
-      processPageLoadEv();
-    });
+  // Handle focus change events
+  // TODO.
+
+  // Handle page loads
+  $('#content_frame').bind('beforeunload', function() {
+    processPageUnloadEv();
+  });
+  $('#content_frame').on('load', function() {
+    processPageLoadEv();
+  });
 
 }
 
@@ -38,10 +39,18 @@ function loadInit() {
 // Helper functions
 function processInputEv(e) {
   console.log("INPUT event caught!");
+
+  // Find the currently active element in the iframe
+  var activeElem = $($('#content_frame').contents()[0].activeElement);
+  if( activeElem.val() != '' ) {
+    // Log the new value
+  }
+
+
 }
 
 function processPageLoadEv() {
-  alert("LOADED!");
+  //alert("LOADED!");
   prevPage = curPage;
   curPage = $('#content_frame').contents()[0].location.href;
   console.log("Page changed from: " + prevPage + " --> " + curPage);
